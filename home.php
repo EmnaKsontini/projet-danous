a16b9ea037ed53dd9d96c022d6f394ed52a9e3dd
Rym <rymoucha01@gmail.com>
2018-04-14 18:13:44 +0100

    stena

--- home.php
+++ home.php
@@ -38,7 +38,7 @@
         <div class="top_head">
             <div class="couverture ">
                 <a href="https://www.script-tutorials.com/">
-                    <img src="images/logo.png" title="E-Store template" alt="E-Store template" />
+                    <img src="images/logo.png" title="logo" alt="logo" />
                 </a>
             </div>
 
