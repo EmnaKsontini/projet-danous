c178d2091d2d6c9477c62cceb6b5f291cb92ea03
meriembenchaaben <meriembenchaaben97@gmail.com>
2018-04-14 11:29:48 +0100

    first Ever , without BOOTSTRAP

--- first.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/** it's quite
- * Created by PhpStorm.
- * User: Meriem
- * Date: 10/04/2018
- * Time: 13:08
- */
-echo "try";
--- hello.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "helloWorld";
-
-echo 'test';
-echo " c'est Nacir" ;
