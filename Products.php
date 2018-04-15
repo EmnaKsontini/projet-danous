3a7325273ea54a94a92c3d66838c4bf073041206
nezz7 <913408OMARKAMOUN>
2018-04-15 13:43:43 +0200

    count() dans Filtre

--- Products.php
+++ Products.php
@@ -34,7 +34,12 @@
                 // cette fonction permet d eliminer les attributs qu on va pas utiliser dans le filtre--
                 function verifierFiltre ($s){
                   return (strcmp($s,"Reference")!=0 &&strcmp($s,"Grantie")!=0 && strcmp($s,"Connecteurs")!=0 && strcmp($s,"stock")!=0  );
-              }
+                }
+                //cette fonction permet de corriger la requete lorsque on utilise le caractere spécialte " -> \"
+                function verif_carct_sep ($s){
+                    $ret=str_replace("\"","\\\"",$s);
+                    return $ret;
+                }
                 try {
                    $bdd = new
                   PDO('mysql:host=localhost;dbname=danousdatabase', 'root', '');
@@ -86,17 +91,22 @@
                 foreach ($tab as $a) {
                     if (verifierFiltre($a->Field)){
                         echo ("<h4>".$a->Field."</h4>");
-                        $req2 = "SELECT DISTINCT `".$a->Field."` FROM pc";
-                        $query2 = $bdd -> query($req2);
+                        $req_dist_element = "SELECT DISTINCT `".$a->Field."` FROM pc";
+                        $query_dist_element= $bdd -> query($req_dist_element);
                         try {
-                            $values = $query2->fetchAll(PDO::FETCH_OBJ);
+                            $values = $query_dist_element->fetchAll(PDO::FETCH_OBJ);
                         } catch (Exception $e) {
                             echo 'Exception reçue : ',  $e->getMessage(), "\n";
                         }
                         echo  "<form>";
                         foreach ($values as $val){
                             $key=$a->Field;
-                            echo ("<input type=\"checkbox\" id=\"vehicle1\" value=\"".$val->$key."\">".$val->$key."<br>");
+                            $f=verif_carct_sep($val->$key);
+                            $req_count="SELECT COUNT(*) FROM pc where `".$key."`=\"".$f."\"";
+                            $query_count=$bdd->query($req_count);
+                            $count=$query_count->fetch();
+
+                            echo ("<input type=\"checkbox\" id=\"vehicle1\" value=\"".$val->$key."\">".$val->$key." (".$count[0].")<br>");
                         }
                         echo "</form>";
                     }
