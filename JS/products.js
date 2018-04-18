7868fe3f1d4945467e9105a5d5257e730bd5330d
nezz7 <913408OMARKAMOUN>
2018-04-18 23:50:58 +0100

    Filtre works AJAX JQuery used

--- JS/products.js
+++ JS/products.js
@@ -1,21 +1,31 @@
 var checkbox = document.getElementsByTagName("input");
+var divLeft= document.getElementById("left");
 
 var i;
 var notChecked=0;
+function generate_query () {
+     str = "";
+    for (i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            str += "\`" + checkbox[i].name + "\`=\"" + checkbox[i].id + "\" or ";
+        }
+    }
+    return str.substr(0,str.length-3);
+}
 for (i=0;i<checkbox.length;i++){
     checkbox[i].addEventListener('click', function () {
-        if (this.type=="checkbox") {
-            if (this.checked) {
-                console.log(this.id);
-                console.log(this.name);
-                notChecked--;
-            }else{
-                notChecked++;
-            }
+        var str=generate_query();
+        var url = "filtre.php";
+            $.post("filtre.php",
+                {
+                    req: str
+                },
+                function(data, status){
+                   console.log ("Status: " + status);
+                   divLeft.innerHTML=data;
 
+                });
 
-            console.log(notChecked);
-        }
 
     });
 }
@@ -24,3 +34,4 @@ for (i=0;i<checkbox.length;i++){
         notChecked++;
 }
 console.log(notChecked);
+
