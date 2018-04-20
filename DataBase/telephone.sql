b7e0b992dcc44881b852006b484bc0cf5bfa7466
Mehrez-Bouasker <mehrez.bouasker@gmail.com>
2018-04-20 23:14:16 +0100

    update image names

--- DataBase/telephone.sql
+++ DataBase/telephone.sql
@@ -51,14 +51,15 @@ CREATE TABLE IF NOT EXISTS `telephone` (
 --
 
 INSERT INTO `telephone` (`Reference`, `Categorie`, `Marque`, `Prix`, `Stock`, `Garantie`, `Dimension`, `Capacite de stockage`, `RAM`, `Appareil Photo Arriere`, `Appareil photo fronatle`, `Resolution`, `Batterie`, `Frequence du processeur`) VALUES
-('Galaxy_S8_plus', 'Smartphone', 'Samsung', 2500, 10, 1, '159.5 x 73.4 x 8.1 mm', '64 Go', '4 Go', '12MP', '8MP', '1440 x 2960 pixels', '3500mAh', '2.3 GHz'),
-('Galaxy_S8', 'Smartphone', 'Samsung', 2200, 8, 1, '148.9 x 68.1 x 8 mm', '64 Go', '4 Go', '12MP', '8MP', '1440 x 2960 pixels', '3000mAh', '2.3 GHz'),
-('Samsung_Galaxy_J5', 'Smartphone', 'Samsung', 500, 50, 1, '145.8 x 72.3 x 8.1 mm', '16 Go', '2 Go', '13MP', '5MP', '720 x 1280 pixels', '3100mAh', '1.2 GHz'),
-('Mate_9', 'Smartphone', 'Huawei', 1800, 4, 1, '156.9 x 78.9 x 7.9 mm', '64 Go', '4 Go', 'Dual 20 MP +12 MP', '8MP', '1080 x 1920 pixels', '4000mAh', '2.3 GHz'),
-('P9_Lite', 'Smartphone', 'Huawei', 600, 2, 1, '146.80 x 72.60 x 7.50 mm', '16 Go', '3 Go', '13MP', '8MP', '1080 x 1920 pixels', '3000mAh', '1.2 GHz'),
-('Iphone_8_PLUS', 'Smartphone', 'Apple', 2800, 2, 1, '158,4 x 78,1 x 7,5 mm', '64/256 Go', '3 Go', '12MP', '7MP', '1920 x 1080 pixels', '2691 mAh', '2.4 GHz'),
-('Iphone_7', 'Smartphone', 'Apple', 2500, 4, 1, '138.3 x 67.1 x 7.1 mm', '128 Go', '2 Go', '12MP', '7MP', '750 X 1334 pixels', '1960mAh', '2.34 GHz');
+('Galaxy S8 plus', 'Smartphone', 'Samsung', 2500, 10, 1, '159.5 x 73.4 x 8.1 mm', '64 Go', '4 Go', '12MP', '8MP', '1440 x 2960 pixels', '3500mAh', '2.3 GHz'),
+('Galaxy S8', 'Smartphone', 'Samsung', 2200, 8, 1, '148.9 x 68.1 x 8 mm', '64 Go', '4 Go', '12MP', '8MP', '1440 x 2960 pixels', '3000mAh', '2.3 GHz'),
+('Samsung Galaxy J5', 'Smartphone', 'Samsung', 500, 50, 1, '145.8 x 72.3 x 8.1 mm', '16 Go', '2 Go', '13MP', '5MP', '720 x 1280 pixels', '3100mAh', '1.2 GHz'),
+('Mate 9', 'Smartphone', 'Huawei', 1800, 4, 1, '156.9 x 78.9 x 7.9 mm', '64 Go', '4 Go', 'Dual 20 MP +12 MP', '8MP', '1080 x 1920 pixels', '4000mAh', '2.3 GHz'),
+('P9 Lite', 'Smartphone', 'Huawei', 600, 2, 1, '146.80 x 72.60 x 7.50 mm', '16 Go', '3 Go', '13MP', '8MP', '1080 x 1920 pixels', '3000mAh', '1.2 GHz'),
+('Iphone 8 PLUS', 'Smartphone', 'Apple', 2800, 2, 1, '158,4 x 78,1 x 7,5 mm', '64/256 Go', '3 Go', '12MP', '7MP', '1920 x 1080 pixels', '2691 mAh', '2.4 GHz'),
+('Iphone 7', 'Smartphone', 'Apple', 2500, 4, 1, '138.3 x 67.1 x 7.1 mm', '128 Go', '2 Go', '12MP', '7MP', '750 X 1334 pixels', '1960mAh', '2.34 GHz');
 COMMIT;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
