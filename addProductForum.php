d3fff2587c2c0023bf37ea05c3eb341d09d04213
nezz7 <913408OMARKAMOUN>
2018-04-28 22:29:53 +0100

    80% add Product

---
+++ addProductForum.php
@@ -0,0 +1,28 @@
+<section id="settings" >
+    <p>Add new Product :</p>
+
+    <form action="AddProduct.php" method="post" >
+    <?php
+
+    //retourne le type d un obj
+    function typeObj ($obj){
+          if (strcmp($obj->Type,"int(11)")==0)
+            return "number";
+        return "text";
+    }
+    try {
+    $bdd = new PDO('mysql:host=localhost;dbname=danousdatabase', 'root', '');
+    }catch (PDOException $e){
+    print "Erreur : " . $e->getMessage();
+    die();
+    }
+    $req = "DESCRIBE ".$_POST["tab"];
+    $query = $bdd -> query($req);
+    $attributs= $query -> fetchAll(PDO::FETCH_OBJ);
+    foreach ($attributs as $att)
+     echo '<p class="setting"><span>'.$att->Field.'</span> <input type="'.typeObj($att).'"  name="'.$att->Field.'"/></p>';
+    ?>
+
+<button class="submit-btn" name="table" value="<?php echo $_POST["tab"]?>" >OK</button>
+    </form>
+</section>
