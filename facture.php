eb571fe7282dc141aa318c07a4539b2f60af34dc
Emna <ksontiniaa@gmail.com>
2018-04-29 15:46:05 +0100

    facturation loading

--- facture.php
+++ facture.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <?php session_start();
+
+if (isset($_SESSION["login"]))
+$log=$_SESSION["login"];
+{
+    $req = "SELECT * FROM autho WHERE login= \"" . $log . "\"";
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=danousdatabase', 'root', '');
+        $stmt = $pdo->query($req);
+        $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        exit('<b>Catched exception at line ' . $e->getLine() . ' (code : ' . $e->getCode() . ') :</b> ' . $e->getMessage());
+    }
+
+    $point = $rows["pointdanous"];
+}
+
  ?>
+
 <html>
 <head>
 <title> Payment </title>
 <link href="style1.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
-	<div class="main">	
-
+<script></script>
+	<div class="main">
 		<div class="div1">
 			<div class="div1_left">
 				<div class="div1_left1">
@@ -37,6 +54,7 @@
                             if ($key1 == "quantity") {
                                 echo "<br><li> quantity : $value1</li></ul> <hr> ";
                                 $_SESSION["total"] = $_SESSION["total"] + $value1 * $prix;
+
                             }
                         }
 
@@ -47,6 +65,20 @@
 
 				</div>
 				<div class="total">
+                    <?php
+                    if (isset($_GET["click"])) {
+
+                        $_SESSION["total"] = $_SESSION["total"]- $_SESSION["total"] * ($point/100);
+
+                        $req=" UPDATE `autho` SET  pointdanous= '0' WHERE login= \"".$log."\"";
+                       $stmt1 = $pdo->query($req);
+                       $rows1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+
+
+                    }
+
+
+                    ?>
 					<h3> Total Price : <?php echo $_SESSION["total"]?> DT  </h3>
                     <p class="comment">Price includes all taxes</p>
 				</div>
@@ -103,7 +135,9 @@
 								</form>
 							</div>
 							<div class="div2_right2">
-								<p class="text"> Click here to Use your <img class="logo" src="images/logo.png"  width="100px" height="50px" > points to get a discount ( you already have  point ) <br> <br>  <button class="button4"> cool ! <span><i aria-hidden="true"></i></span></button></p>
+                                <p class="text"> Click here <a href="?click=1"><img class="logo" src="images/logo.png" width="100px" height="50px"  border="0.1px" ></img></a>to Use your points to get a discount ( you already have <?php  echo $point ?> points ) <br> <br></p>
+
+
 
 								</form>
 							</div>
